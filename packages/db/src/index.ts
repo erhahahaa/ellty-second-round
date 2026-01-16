bbb3172493fd41bb3891562b2dec5e4408be6059
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2026-01-16 07:40:33 +0700

    feat(db): add database schema with Drizzle ORM

---
+++ packages/db/src/index.ts
@@ -0,0 +1,29 @@
+import { env } from "@ellty-second-round/env/server";
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import ws from "ws";
+
+import * as schema from "./schema";
+
+let connectionString = env.DATABASE_URL;
+
+if (env.NODE_ENV === "development") {
+	connectionString = "postgres://postgres:postgres@db.localtest.me:5432/main";
+	neonConfig.fetchEndpoint = (host) => {
+		const [protocol, port] =
+			host === "db.localtest.me" ? ["http", 4444] : ["https", 443];
+		return `${protocol}://${host}:${port}/sql`;
+	};
+	const connectionStringUrl = new URL(connectionString);
+	neonConfig.useSecureWebSocket =
+		connectionStringUrl.hostname !== "db.localtest.me";
+	neonConfig.wsProxy = (host) =>
+		host === "db.localtest.me" ? `${host}:4444/v2` : `${host}/v2`;
+}
+neonConfig.webSocketConstructor = ws;
+
+console.log("Databer URL", connectionString);
+
+const sql = neon(connectionString);
+
+export const db = drizzle({ client: sql, schema });
